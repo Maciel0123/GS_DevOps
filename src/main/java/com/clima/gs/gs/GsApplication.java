fe8053e7098e9298eed1adc52941a409b09194d5
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-06-01 19:36:06 -0300

    fix: main variables

--- src/main/java/com/clima/gs/gs/GsApplication.java
+++ src/main/java/com/clima/gs/gs/GsApplication.java
@@ -1,6 +1,5 @@
 package com.clima.gs.gs;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,11 +8,9 @@ public class GsApplication {
 
 	public static void main(String[] args) {
 
-		Dotenv dotenv = Dotenv.configure().load();
-
-		System.setProperty("DB_URL", dotenv.get("DB_URL"));
-		System.setProperty("DB_USER", dotenv.get("DB_USER"));
-		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("DB_URL", System.getenv("DB_URL"));
+		System.setProperty("DB_USER", System.getenv("DB_USER"));
+		System.setProperty("DB_PASSWORD", System.getenv("DB_PASSWORD"));
 
 		SpringApplication.run(GsApplication.class, args);
 	}
