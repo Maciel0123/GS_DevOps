ab0a12830482f7e7336f30f77117be1f210946f9
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-06-01 19:07:47 -0300

    feature: main with .env for bd

--- src/main/java/com/clima/gs/gs/GsApplication.java
+++ src/main/java/com/clima/gs/gs/GsApplication.java
@@ -1,5 +1,6 @@
 package com.clima.gs.gs;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GsApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure().load();
+
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USER", dotenv.get("DB_USER"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(GsApplication.class, args);
 	}
-
 }
