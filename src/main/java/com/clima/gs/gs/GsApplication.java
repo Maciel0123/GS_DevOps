75b2e8ad74ef265796e744bf7bd4d17d47dd5db9
Mariana Christina <161539215+MaryChriss@users.noreply.github.com>
2025-06-03 15:15:19 -0300

    remove variables

--- src/main/java/com/clima/gs/gs/GsApplication.java
+++ src/main/java/com/clima/gs/gs/GsApplication.java
@@ -1,6 +1,5 @@
 package com.clima.gs.gs;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -10,13 +9,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class GsApplication {
 
     public static void main(String[] args) {
-
-        Dotenv dotenv = Dotenv.configure().load();
-
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USER", dotenv.get("DB_USER"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-
         SpringApplication.run(GsApplication.class, args);
     }
 }
